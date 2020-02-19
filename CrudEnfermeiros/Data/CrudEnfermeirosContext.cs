e8b4cbc52213bdf7cdb0c2cdae90aa1f2ceb0bb4
Carlos Eduardo <cadu_medeirossantos2011@hotmail.com>
2020-02-19 16:35:12 -0300

    Acesso a banco de dados implementado

---
+++ CrudEnfermeiros/Data/CrudEnfermeirosContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudEnfermeiros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudEnfermeiros.Data
+{
+    public class CrudEnfermeirosContext : DbContext
+    {
+        public CrudEnfermeirosContext(DbContextOptions<CrudEnfermeirosContext> options) : base(options)
+        {
+        }
+
+        DbSet<Hospital> Hospitals { get; set; }
+        DbSet<Enfermeiro> Enfermeiros { get; set; }
+    }
+}
